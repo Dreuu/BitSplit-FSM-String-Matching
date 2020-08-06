6c8b206bc10dd10479ae447e5788fdf94e1ab68d
Dreuu <41088104+Dreuu@users.noreply.github.com>
2020-08-06 13:15:58 -0400

    Separate FSM bit dictionaries
    Fixed an issue where the 8 FSM dictionaries would each be the same

--- psuedo_test.py
+++ psuedo_test.py
@@ -2,29 +2,31 @@ def count(d):
 	return sum([count(v)+1 if len(v) > 0 else 1 for v in d.values()])
 	
 def count_nodes(pats):
-	prefixes = [{}]*8
+	prefixes = [dict() for _ in range(8)]
 	
-	for pat in pats:
-		for char_idx in range(len(pat)):
-			char_bits = bin(int.from_bytes(pat[char_idx].encode(), 'big'))
+	for pat_idx in range(len(pats)):
+		char_bit_arr = []
+		for char_idx in range(len(pats[pat_idx])):
+			char_bits = bin(int.from_bytes(pats[pat_idx][char_idx].encode(), 'big'))
 			char_bits = str(char_bits)[2:]
 			if len(char_bits) < 8:
 				char_bits = '0'*(8-len(char_bits)) + char_bits
+			char_bit_arr.append(char_bits)
 			for bit_idx in range(8):
 				cur_char = char_bits[bit_idx]
 				if char_idx == 0:
 					if cur_char not in prefixes[bit_idx]:
-						prefixes[bit_idx][cur_char] = {}
+						prefixes[bit_idx][cur_char] = dict()
 						continue
-				dict = prefixes[bit_idx]
+						
+				t_dict = prefixes[bit_idx]
 				for i in range(char_idx):
-					dict = dict[char_bits[i]]
-				if cur_char not in dict:
-					dict[cur_char] = {}
+					t_dict = t_dict[char_bit_arr[i][bit_idx]]
+				if cur_char not in t_dict:
+					t_dict[cur_char] = dict()
 	
-	[[print(str(bin(int.from_bytes(char.encode(), 'big')))[2:]) for char in pat] for pat in pats]
-	#print(prefixes)
-	[print(x) for x in prefixes]
+	#[[print(str(bin(int.from_bytes(char.encode(), 'big')))[2:]) for char in pat] for pat in pats]
+	#[print(x) for x in prefixes]
 	
 	return sum([count(x) for x in prefixes])
 	
@@ -68,18 +70,26 @@ def str_grp(strings, num_groups):
 				correlations = [correlation(group, strings[i]) for group in groups]
 				correlations.sort()
 				corr_diffs[i] = correlations[-1] - correlations[-2]
-		most_difference_idx = corr_diffs.index(max(corr_diffs))		
-		correlations = [correlation(group, strings[most_difference_idx]) for group in groups]
-		groups[correlations.index(max(correlations))].append(strings[most_difference_idx])
+		if max(corr_diffs) != 0:
+			most_difference_idx = corr_diffs.index(max(corr_diffs))		
+			correlations = [correlation(group, strings[most_difference_idx]) for group in groups]
+			groups[correlations.index(max(correlations))].append(strings[most_difference_idx])
+		else:
+			for string in strings:
+				if not used(groups, string):
+					correlations = [correlation(group, string) for group in groups]
+					groups[correlations.index(max(correlations))].append(string)
 		
 	return groups
 	
 	
 if __name__=='__main__':
-	#groups = str_grp(['me', 'him', 'he', 'his'], 2)
-	groups = str_grp(['me', 'him', 'he', 'his', 'dog', 'cat'], 2)
+	patterns = ['me', 'him', 'his', 'he']
+	groups = str_grp(patterns, 2)
 	
-	print(count_nodes(groups))
+	print(patterns)
+	print(count_nodes(patterns))
+	print()
 	
 	for group in groups:
 		print(group)
